f7c0a69211eca0be6b68b38cc2121b464e7d7db5
allisonpenko <allison.penko@nrlssc.navy.mil>
2017-01-20 19:23:20 -0600

    fixed file search commands

--- demoKalmanFiltering.m
+++ demoKalmanFiltering.m
@@ -2,16 +2,16 @@
 
 clear
 cBInputPn = '102210cBathys/';     % a simple example day, stored locally
-fns = dir([cBInputPn '*cBathy*']);
+fns = dir([cBInputPn,'*.mat']);
 H = 1;            % assume wave height is 1 m if absence of better data
 
 % Note that we will store the output in a different location for demo
 % purposes.  You would normally save the result back where you found the
 % original cBathy files.
 
-cBOutputPn = '102210cBathysSmoothed/';
+cBOutputPn = '102210cBathysSmoothed';
 if ~exist(cBOutputPn, 'dir')
-    yesNo = mkdir('.', cBOutputPn);
+    yesNo = mkdir(cBOutputPn);
     if ~yesNo
         error('Unable to create output directory')
     end
@@ -21,17 +21,17 @@ end
 % directory (only for demo).  Note that Kalman will start with second
 % collect (averaging with first)
 for i = 2: length(fns)
-    load([cBInputPn fns(i-1).name])
+    load([cBInputPn, fns(i-1).name])
     priorBathy = bathy;
-    load([cBInputPn fns(i).name])
+    load([cBInputPn, fns(i).name])
     bathy = KalmanFilterBathyNotCIL(priorBathy, bathy, H);
-    eval(['save ' cBOutputPn fns(i).name ' bathy'])
+    eval(['save ' cBOutputPn,filesep, fns(i).name, ' bathy'])
 end
 
 % display the results
-fns = dir([cBOutputPn '*cBathy*']);
+fns = dir([cBOutputPn,filesep,'*.mat']);
 for i = 1: length(fns)
-    load([cBOutputPn fns(i).name])
+    load([cBOutputPn, filesep, fns(i).name])
     plotBathyCollectKalman(bathy)
     pause
 end
