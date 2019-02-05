6f1dd4345b3d4396f4b0cef02fe162908a6ae82e
John Stanley <stanley@coas.oregonstate.edu>
2019-02-04 17:02:31 -0800

    2017 Rob update

---
+++ analyzeSingleBathyRunFrommBW.m
@@ -0,0 +1,38 @@
+function bathy = analyzeSingleBathyRunFrommBW(mBWPathname, n)
+%
+%  bathy = analyzeSingleBathyRunFrommBW(mBWPathname, n)
+%
+%  simple run of analyzeBathyCollect for a single stackName.  Useful for
+%  debugging
+
+eval(n.station)
+bathy.epoch = n.time; bathy.sName = mBWPathname; bathy.params = params;
+load(mBWPathname)
+bathy.h0.x = params.xyMinMax(1):params.dxm:params.xyMinMax(2);
+bathy.h0.y = params.xyMinMax(3):params.dym:params.xyMinMax(4);
+bathy.h0.h = findh0Duck(bathy.h0.x, bathy.h0.y, 1);   % seed depths for search
+bathy = analyzeBathyCollect(XYZ, T(:), RAW, CAM, bathy);
+
+%   Copyright (C) 2017  Coastal Imaging Research Network
+%                       and Oregon State University
+
+%    This program is free software: you can redistribute it and/or
+%    modify it under the terms of the GNU General Public License as
+%    published by the Free Software Foundation, version 3 of the
+%    License.
+
+%    This program is distributed in the hope that it will be useful,
+%    but WITHOUT ANY WARRANTY; without even the implied warranty of
+%    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+%    GNU General Public License for more details.
+
+%    You should have received a copy of the GNU General Public License
+%    along with this program.  If not, see
+%                                <http://www.gnu.org/licenses/>.
+
+% CIRN: https://coastal-imaging-research-network.github.io/
+% CIL:  http://cil-www.coas.oregonstate.edu
+%
+%key cBathy
+%
+
