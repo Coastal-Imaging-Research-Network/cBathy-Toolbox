0267312c1dd87930fe3f75df20bc81a82c8d2d13
John Stanley <stanley@coas.oregonstate.edu>
2019-02-04 16:56:45 -0800

    2017 Rob update

--- csmInvertKAlpha.m
+++ csmInvertKAlpha.m
@@ -1,8 +1,8 @@
-function fDependent = ...
-    csmInvertKAlpha(f, G, xyz, xm, ym, params, kappa)
+function [fDependent, camUsed] = ...
+    csmInvertKAlpha(f, GAll, xyz, cam, xm, ym, bathy)
 %
 % fCombined = ...
-%   csmInvertKAlpha(f, G, xyz, xm, ym, params, kappa)
+%   csmInvertKAlpha(f, G, xyz, cam, xm, ym, bathy)
 %
 % estimate wavennumber components from cross spectral matrix CSM
 % using local nolinear model for any provided subset of Fourier values
@@ -12,9 +12,9 @@ function fDependent = ...
 %   f   frequencies of input Fourier coefficients, Nf x 1
 %   G, Fourier coeffients from a tiles worth of pixels, Nxy x 1 (complex)
 %   xyz, pixel locations (Nxy of them; z column is not required
+%   cam, camera number for each xyz, required for handling seams
 %   xm, ym, single location for this analysis
-%   params, parameters structure for analysis (from SETTINGS files)
-%   kappa, spatially-variable samping domain scaling
+%   bathy, building results structure with everything in it.
 % Outputs:
 %   fDependent: small structure of results with the following fields, each
 %   of length nKeep (one for each frequency)
@@ -24,102 +24,89 @@ function fDependent = ...
 %       - skill dof, - skill and degrees of freedom
 
 % 03/07/10 - Holman fix to catch edge error where a tomographic point
-%   an extrapolation region can get poor estimates with low error
+%   an extrapolation region can get poor eXYZstimates with low error
 %   because only a very few pixels are fit well.  Fix is to require
 %   nGood pixels (likely > 3) and to have max(deltax) and max(deltay)
 %   at least wavelengthFraction of the expected wavelength.
 % 09/01/11 - Holman rewrite to finally get rid of tiling and analyze a
 %   single location only
 % 12/05/11 - Holman major change to base on EOFs rather than direct from CSM
-% 5/19/2018  - Palmsten add option to use Richie Slocum's nonlinear fit
-% instead of nlinfit.m from Matlab Statistics and Machine Learning Toolbox
+% 05/22/17 - Holman major change to dynamic tile size (based on fB) and new
+%               seed based on initial guess at bathy, bathy.h0
 
 %% %%  1. constants and parameters
+
 clear fDependent
 g=9.81;                         % 'g'
 minNeededPixels = 6;            % to avoid edge anomalies
 minLFraction = 0.5;             % min fract of wavelength to span
-maxAlphaOffset = pi/2;          % max radian distance from expected wave direction
+kL = 1.25;                      % goal tile size compared to h0 wavelength
+
+ri = [0:0.01:1]';       % create a hanning filter for later interp
+ai = (1 - cos( pi*(0.5 + 0.5*ri) ).^2);
 
 OPTIONS = statset('nlinfit');   % fit options
 OPTIONS.MaxIter = 50;
 OPTIONS.TolFun = 1e-6; %%%1e-3;
 
-Nxy = size(xyz,1); 
-if Nxy<minNeededPixels          % too few points, bail out
-    if( cBDebug( params ) )
-        fprintf('Inadequate data to analyze tile, [x,y] = [%d, %d]\n', xm, ym)
-    end;
-    fDependent.k = nan(1,params.nKeep);
-    return
-end
+% find the dominant frequencies for this location based on the standard
+% tile size Lx by Ly (passed in bathy).
 
-ri = [0:0.01:1]';       % create a hanning filter for later interp
-ai = (1 - cos( pi*(0.5 + 0.5*ri) ).^2);
-
-%%%% 2. find basic csm for all fB frequencies, sort by total coh^2 into preferred
-%%%% order and keep only nKeep frequencies
-fB = params.fB;
-nKeep = params.nKeep;
-for i = 1: length(fB)
-    id = find(abs(fB(i)-f) < (fB(2)-fB(1))/2);     % find the f's that match
-    % pull out the fft data and do the cross spectrum
-    C(:,:,i) = G(id,:)'*G(id,:) / length(id);   % pos 2nd leads 1st.
-end
-
-% create the coherence squared for all the potential frequency bands
-coh2 = squeeze(sum(sum(abs(C)))/(size(xyz,1)*(size(xyz,1)-1)));
-[~, coh2Sortid] = sort(coh2, 1, 'descend');  % sort by coh2
-fs = fB(coh2Sortid(1:nKeep));         % keep only the nKeep most coherent
+fs = findDominantfBs(f, GAll, xyz, cam, xm, ym, bathy);
 fDependent.fB = fs;
-C = C(:,:,coh2Sortid(1:nKeep));      % ditto
+xyAll = xyz(:,1:2);
 
-% report frequencies we keep
-% formatStr = repmat('%.3f ', 1, nKeep);
-% fprintf(['Frequencies [' formatStr '] Hertz\n'], fs);
-
-%% %%  3. Find lags and weightings.  NOTE that we no longer rotate
-%% coordinates to a user cross-shore orientation.  The user is now
-%% responsible for providing cross-shore oriented coord system.
-
-xy = xyz(:,1:2);
-% find lags xym and pixels for weighting
-dxmi = xy(:,1) - repmat(xm, Nxy, 1);
-dymi = xy(:,2) - repmat(ym, Nxy, 1);
-r = sqrt((dxmi/(params.Lx*kappa)).^2 + (dymi/(params.Ly*kappa)).^2);
-Wmi = interp1(ri,ai,r,'linear*',0);  % sample normalized weights
-
-% find span of data in x and y to determine if sufficient
-maxDx = max(max(repmat(xy(:,1),1,Nxy) - repmat(xy(:,1)',Nxy,1)));
-maxDy = max(max(repmat(xy(:,2),1,Nxy) - repmat(xy(:,2)',Nxy,1)));
-
-% calculate the distance from every point to every other point
-
-%% %%  4. loop through frequencies from most to least coherent.  For each
-%% f, find the csm, then the first EOF.  If sufficiently coherent, fit to
+%% %%  Loop through frequencies from most to least coherent.  For each
+%% f, find the frequency-dependent tile, then find the csm, then the 
+%% first EOF.  If sufficiently coherent, fit to
 %% find the k, alpha and scalar phase (no lasting value).
 
-% starting search seed for alpha (should this be ZERO after rotation?)
-seedAlpha = params.offshoreRadCCWFromx;
-
-for i = 1:nKeep         % frequency loop
-    % prepare nlinfit params.  Note that k is radial wavenumber
-    kmin = (2*pi*fs(i))^2/g; % smallest  and largest wavenumber
-    kmax = 2*pi*fs(i)/sqrt(g*params.MINDEPTH); 
-    LExpect = minLFraction*4*pi/(kmin+kmax);     % expected scale from mean k.
-    LB_UB = [kmin; kmax];
-    OPTIONS.TolX = min([kmin/1000,pi/180/1000]); % min([kmin/100,pi/180/10]);
-    statset(OPTIONS);
-    warning off stats:nlinfit:IterationLimitExceeded
-
+for i = 1:length(fs)         % frequency loop
+    
     % info for depth subsequent h error estimation
     hiimax = 9.8*(1/fs(i)^2)/(2*pi)/2;  % deepest allowable = L0/2
-    hii = [params.MINDEPTH:.1:hiimax]'; % find k for full range of h
+    hii = [bathy.params.MINDEPTH:.1:hiimax]'; % find k for full range of h
     kii = dispsol(hii, fs(i),0);
+    
+    % estimate the rough depth, h0 then cull tile to kL times expected
+    % wavelength.  Then decimate to maxNPix
+    h0 = interp2(bathy.h0.x, bathy.h0.y, bathy.h0.h, xm, ym);
+    LBoxx = kL * 2*pi/interp1(hii,kii,h0);          % determine reqd tile size
+    LBoxy = LBoxx *bathy.params.Ly/bathy.params.Lx; 
+    [G, xy, camUsed] = ...
+                spatialLimitBathy(GAll, xyAll, cam, xm, ym, ...
+                LBoxx/2, LBoxy/2, bathy.params.maxNPix);
+    Nxy = size(xy,1);
+    
+    % compute the CSM
+    id = find(abs(fs(i)-f) < (bathy.params.fB(2)-bathy.params.fB(1))/2);     % find the f's that match
+    C = G(id,:)'*G(id,:) / length(id);   % pos 2nd leads 1st.
+ 
+    % weightings
+    dxmi = xy(:,1) - repmat(xm, Nxy, 1);
+    dymi = xy(:,2) - repmat(ym, Nxy, 1);
+    r = sqrt((dxmi/(bathy.params.Lx*bathy.kappa)).^2 + (dymi/(bathy.params.Ly*bathy.kappa)).^2);
+    Wmi = interp1(ri,ai,r,'linear*',0);  % sample normalized weights
 
-    % pull out csm
-    Cij = C(:,:,i);
-    [v,d] = eig(Cij);
+    % find span of data in x and y to determine if sufficient
+    maxDx = max(max(repmat(xy(:,1),1,Nxy) - repmat(xy(:,1)',Nxy,1)));
+    maxDy = max(max(repmat(xy(:,2),1,Nxy) - repmat(xy(:,2)',Nxy,1)));
+
+    % prepare nlinfit params.  Note that k is radial wavenumber
+    kmin = (2*pi*fs(i))^2/g; % smallest  and largest wavenumber
+    kmax = 2*pi*fs(i)/sqrt(g*bathy.params.MINDEPTH); 
+    LExpect = LBoxx/kL;     % expected wavelength
+    seedAlpha = bathy.params.offshoreRadCCWFromx;
+    LB_UB = [kmin seedAlpha-pi/2;kmax seedAlpha+pi/2];
+    OPTIONS.TolX = min([kmin/1000,pi/180/1000]); % min([kmin/100,pi/180/10]);
+    statset(OPTIONS);
+    widILE = 'stats:nlinfit:IterationLimitExceeded';
+    widRD = 'stats:nlinfit:RankDefficient';
+    warning('off', widILE)
+    warning('off', widRD)
+
+    % find the eigenvector
+    [v,d] = eig(C);
     lam = real(diag(d));  % order eigenvalues and remove teeny imag component
     [lam1, lamInd] = max(lam);
     lam1Norm = lam1/sum(lam)*length(lam);  % as ratio to uniform eigenvalues
@@ -127,62 +114,50 @@ for i = 1:nKeep         % frequency loop
     w = abs(v).*Wmi;           % final weights for fitting.
     
     % check if sufficient data quality and xy span
-    if ~((lam1Norm>params.minLam) && (maxDx>LExpect) && (maxDy>LExpect))
+    if ~((lam1Norm>bathy.params.minLam) && (maxDx>LExpect) && (maxDy>LExpect))
         kAlphaPhi = [nan nan nan];
         ex = kAlphaPhi;
         skill = nan;
     else
         try
-            % do nonlinear fit on surviving data
-            kAlphaPhiInit = findKAlphaPhiInit(v, xy, LB_UB, params);
-
-            if params.nlinfit == 1 & ~isempty(ver('stats')) % use the stats toolbox if you have it
-                [kAlphaPhi,resid,jacob] = nlinfit([xy w], [real(v); imag(v)],...
-                    'predictCSM',kAlphaPhiInit, OPTIONS);
-            elseif params.nlinfit == 0 | ~isempty(ver('stats')) % if you don't have the stats toolbox, or you don't want to use it
-                [kAlphaPhi,ssq,cnt, res, XY,A,resid] = LMFnlsq('res',kAlphaPhiInit,[xy w], [real(v); imag(v)], 'Display',0);
-                kAlphaPhi = kAlphaPhi';
-            end
+            % do nonlinear fit on surviving data.
+            % use first point in eigenvector for phiInit
+            k = 2*pi/LExpect; 
+            alpha = findAlpha0(xy,v);
+            phi = angle(v(1) - k*cos(alpha)*xy(1,1) - k*sin(alpha)*xy(1,2));
+            kAlphaPhiInit = [k alpha phi];
+            [kAlphaPhi,resid,jacob] = nlinfit([xy w], [real(v); imag(v)],...
+                           'predictCSM',kAlphaPhiInit, OPTIONS);
             
             % check if outside acceptable limits
-                        
-            % first wrap alpha offset from seed to the range [-pi, pi)
-            alphaOffsetWrapped = mod((kAlphaPhi(2)-seedAlpha)+pi,2*pi)-pi;
-            % then check if k & alpha are outside acceptable limits
             if ((kAlphaPhi(1)<LB_UB(1,1)) || (kAlphaPhi(1)>LB_UB(2,1)) ...
-                    || abs(alphaOffsetWrapped) > maxAlphaOffset)
+                    || (kAlphaPhi(2)<LB_UB(1,2)) || (kAlphaPhi(2)>LB_UB(2,2)))
                 error;
             end
-            % if good, then wrap alpha to a standard range, [-pi, pi)
-            kAlphaPhi(2) = mod(kAlphaPhi(2)+pi,2*pi)-pi;
             
             % get predictions then skill
             vPred = predictCSM(kAlphaPhi, [xy abs(v)]);
             vPred = vPred(1:end/2) + sqrt(-1)*vPred(1+end/2:end);
             skill = 1 - norm(vPred-v)^2/norm(v-mean(v))^2; % can be neg.
             
-            if( cBDebug( params, 'DOPLOTPHASETILE' ))
+            if( cBDebug( bathy.params, 'DOPLOTPHASETILE' ))
                 figure(i); clf
                 plotPhaseTile(xy,v,vPred)
                 drawnow;
-                fprintf('frequency %d of %d, normalized lambda %.1f\n  ', i, nKeep,lam1Norm)
+                fprintf('frequency %d of %d, normalized lambda %.1f\n  ', ...
+                            i, bathy.params.nKeep,lam1Norm)
             end
-
             % get confidence limits
-            if params.nlinfit == 1 & ~isempty(ver('stats')) % use the stats toolbox if you have it
-                ex = nlparci(real(0*kAlphaPhi),resid,jacob); % get limits not bounds
-                ex = real(ex(:,2)); % get limit, not bounds
-             elseif params.nlinfit == 0 | ~isempty(ver('stats')) % if you don't have the stats toolbox, or you don't want to use it
-                DOF = size(v,1)*2-size(kAlphaPhi,2); % degrees of freedom
-                rmse = norm(resid) / sqrt(DOF);
-                %ex = sigma_p*tstat3( DOF, 1-0.025, 'inv' );
-                ex = rmse*sqrt(diag(inv(A)))*tstat3( DOF, 1-0.025, 'inv' );
-            end
+            ex = nlparci(real(0*kAlphaPhi),resid,jacob); % get limits not bounds
+            ex = real(ex(:,2)); % get limit, not bounds
         catch   % nlinfit failed with fatal errors, adding bogus
             kAlphaPhi = [nan nan nan];
             ex = kAlphaPhi;
             skill = nan;
             lam1 = nan;
+            if( cBDebug( bathy.params, 'DOPLOTPHASETILE' ))
+                figure(i); clf
+            end
         end % try
     end
 
@@ -208,18 +183,20 @@ for i = 1:nKeep         % frequency loop
     end
 end  % frequency loop
 
-if( cBDebug( params, 'DOPLOTPHASETILE' ))
+if( cBDebug( bathy.params, 'DOPLOTPHASETILE' ))
     fDependent
     input('Hit enter to continue ');
 end
 
-%
+warning('on', widILE)
+warning('on', widRD)
+
 %   Copyright (C) 2017  Coastal Imaging Research Network
 %                       and Oregon State University
 
-%    This program is free software: you can redistribute it and/or  
-%    modify it under the terms of the GNU General Public License as 
-%    published by the Free Software Foundation, version 3 of the 
+%    This program is free software: you can redistribute it and/or
+%    modify it under the terms of the GNU General Public License as
+%    published by the Free Software Foundation, version 3 of the
 %    License.
 
 %    This program is distributed in the hope that it will be useful,
